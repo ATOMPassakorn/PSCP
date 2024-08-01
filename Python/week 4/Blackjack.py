579d72a56f8e7d3fca8c52941dd5bc481d466ad3
ATOMPassakorn <atom15440@gmail.com>
2024-08-01 16:08:01 +0700

    Life Is PSCP

---
+++ Python/week 4/Blackjack.py
@@ -0,0 +1,20 @@
+"""Blackjack"""
+def blackjack():
+    """Blackjack"""
+    num=int(input())
+    score=0
+    aces=0
+    for _ in range(num):
+        card=input()
+        if card in ("J","Q","K"):
+            score+=10
+        elif card=="A":
+            score+=11
+            aces+=1
+        elif card.isnumeric():
+            score+=int(card)
+    while score>21 and aces:
+        score-=10
+        aces-=1
+    print(score)
+blackjack()
