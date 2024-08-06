0da9491475accb5bb6ec50002a7c720d12f4ae9a
ATOMPassakorn <atom15440@gmail.com>
2024-08-07 01:11:14 +0700

    Life Is PSCP

--- Python/week 3/Sequence V.py
+++ Python/week 3/Sequence V.py
@@ -2,4 +2,10 @@
 def main():
     """Sequence V"""
     num = int(input())
+    while num:
+        for _ in range(0,7):
+            if num:
+                print(num, end=" ")
+                num-=1
+        print()
 main()
