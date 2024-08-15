d162fcca6d10681a65e07701bf1878aa9fc86d38
ATOMPassakorn <atom15440@gmail.com>
2024-08-15 16:09:27 +0700

    Life Is PSCP

--- Python/week 2/Triangle I.py
+++ Python/week 2/Triangle I.py
@@ -4,11 +4,10 @@ def pythagoras():
     a=float(input())
     b=float(input())
     c=float(input())
-    tri1=abs((a+b)-c)
-    tri2=abs((a+c)-b)
-    tri3=abs((b+c)-a)
-    tri4=abs(c**2-(a**2+b**2))
-    if tri1<=0.01 or tri2<=0.01 or tri3<=0.01 or tri4<=0.01:
+    tri1=abs(c**2-(a**2+b**2))
+    tri2=abs(a**2-(c**2+b**2))
+    tri3=abs(b**2-(a**2+c**2))
+    if tri1<=0.01 or tri2<=0.01 or tri3<=0.01:
         print("Yes")
     else:
         print("No")
