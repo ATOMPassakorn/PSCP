f6c17b3ee9e6519c1d4b82ca6d2b8f0072a3b333
ATOMPassakorn <atom15440@gmail.com>
2024-08-27 14:14:13 +0700

    Life Is PSCP

--- Python/week 3/Grade III.py
+++ Python/week 3/Grade III.py
@@ -1,39 +1,27 @@
 """Grade III"""
-import math
-def main():
+import math as m
+def grade(c):
     """Grade III"""
-    num=int(input())
-    count=0
-    grade=0
-    while count<num:
-        point = float(input())
-        if 100 >= point >= 95:
-            grade+=4
-            count+=1
-        elif 95 > point >= 90:
-            grade+=3.5
-            count+=1
-        elif 90 > point >= 85:
-            grade+=3
-            count+=1
-        elif 85 > point >=80:
-            grade+=2.5
-            count+=1
-        elif 80 > point >= 75:
-            grade+=2
-            count+=1
-        elif 75 > point >= 70:
-            grade+=1.5
-            count+=1
-        elif 70 > point >= 65:
-            grade+=1
-            count+=1
-        elif 65 > point >= 60:
-            grade+=0.5
-            count+=1
-        elif 60 > point >= 0:
-            count+=1
-    ans=grade/num
-    grade=math.floor(ans*100)/100
-    print(f"{grade:.2f}")
-main()
+    score=0
+    for _ in range(c):
+        num=float(input())
+        if 95 <= num <=100:
+            score+=4
+        elif 90 <= num < 95:
+            score+=3.5
+        elif 85 <= num < 90:
+            score+=3
+        elif 80 <= num < 85:
+            score+=2.5
+        elif 75 <= num < 80:
+            score+=2
+        elif 70 <= num < 75:
+            score+=1.5
+        elif 65 <= num < 70:
+            score+=1
+        elif 60 <= num< 65:
+            score+=0.5
+    ans=score/c
+    gpa=m.floor(ans*100)/100
+    print(f"{gpa:.2f}")
+grade(int(input()))
