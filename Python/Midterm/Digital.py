43809ed84ef5f05fb6447f325248b7e614267bdb
ATOMPassakorn <atom15440@gmail.com>
2024-09-05 00:14:53 +0700

    Life Is PSCP

--- Python/Midterm/Digital.py
+++ Python/Midterm/Digital.py
@@ -7,9 +7,9 @@ def digital():
     age=float(input())
     money=float(input())
     widraw=float(input())
-    con1=thai in ("Yes","True") and address in ("Yes","True")
-    con2=age>=16 and widraw<=500000 and money<=840000 and money/12<=70000
-    if con1 and con2:
+    con1=thai in ("Yes","True") and address in ("Yes","True") \
+and age>=16 and widraw<=500000 and money<=840000
+    if con1:
         print(f"Congratulations! {name} is qualified to receive a digital wallet \
 amount of 10,000 baht, sponsored by all taxpayers in Thailand.")
     else:
