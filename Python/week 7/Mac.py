c7c28de4c63588680eacd9d34da376383c16c13e
ATOMPassakorn <atom15440@gmail.com>
2024-09-04 17:20:22 +0700

    Life Is PSCP

--- Python/week 7/Mac.py
+++ Python/week 7/Mac.py
@@ -1,36 +1,28 @@
 """Mac"""
 def mac():
     """mac"""
-    al = "ghijklmnopqrstuvwxyz"
-    spec = "!@#$%^&*()+?_=,<>;/\"\\\'"
-    ad = input()
-    check = True
-    for i in ad:
-        if i.lower() in al or i in spec:
-            check = False
-    if check:
-        if len(ad) == 17:
-            if ad[2] == "-" and ad[5] == "-" and \
-                ad[8] == "-" and ad[11] == "-" and ad[14] == "-":
-                if ad.count("-") == 5 and not ":" in ad and not "." in ad :
-                    print("VALID 1")
-                else:
-                    print("ERROR")
-            elif ad[2] == ":" and ad[5] == ":" and \
-                ad[8] == ":" and ad[11] == ":" and ad[14] == ":":
-                if ad.count("-") == 5 and not "-" in ad and not "." in ad :
-                    print("VALID 2")
-                else:
-                    print("ERROR")
-            else:
-                print("ERROR")
-        elif len(ad) == 14:
-            if ad[4] == "." and ad[9] == "." and ad.count(".") == 2 and not ":" in ad and not "-" in ad :
-                print("VALID 3")
-            else:
-                print("ERROR")
-        else:
-            print("ERROR")
+    alpha="ABCDEFabcdef0123456789-:."
+    text=input()
+    check=True
+    first=len(text)==17 and text[2] == "-" and text[5] == "-" and \
+text[8] == "-" and text[11] == "-" and text[14] == "-" and text.count("-") == 5
+    second=len(text)==17 and text[2] == ":" and text[5] == ":" and \
+text[8] == ":" and text[11] == ":" and text[14] == ":" and text.count(":") == 5
+    third=len(text)==14 and text[4] == "." and text[9] == "." and text.count(".") == 2 \
+and text[:4].isalnum() and text[5:9].isalnum() and text[10:].isalnum()
+    for i in text:
+        if i not in alpha:
+            check=False
+            break
+    if not (len(text)==17 or len(text)==14):
+        check=False
+    if check and (first or second or third):
+        if first:
+            print("VALID 1")
+        elif second:
+            print("VALID 2")
+        if third:
+            print("VALID 3")
     else:
         print("ERROR")
 mac()
