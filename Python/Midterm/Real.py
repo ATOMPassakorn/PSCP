90544d7782ac4553b994ef475da478b2aa8d683a
ATOMPassakorn <atom15440@gmail.com>
2024-09-04 13:09:54 +0700

    Life Is PSCP

--- Python/Midterm/Real.py
+++ Python/Midterm/Real.py
@@ -1,50 +1,51 @@
 """Real"""
 def num1():
     """number input"""
-    ans=""
-    for _ in range(24):
+    binary=""
+    for _ in range(8):
         status=input()
         if status=="on":
-            ans+="1"
+            binary+="1"
         else:
-            ans+="0"
-    return ans
-
-def tran(binary1):
-    """binary"""
-    ans=""
-    for i in range(0, len(binary1), 8):
-        binary_segment=binary1[i:i+8]
-        decimal=""
-        if binary_segment[:7]=="1111110":
-            decimal="0"
-        elif binary_segment[:7]=="0110000":
-            decimal="1"
-        elif binary_segment[:7]=="1101101":
-            decimal="2"
-        elif binary_segment[:7]=="1111001":
-            decimal="3"
-        elif binary_segment[:7]=="0110011":
-            decimal="4"
-        elif binary_segment[:7]=="1011011":
-            decimal="5"
-        elif binary_segment[:7]=="1011111":
-            decimal="6"
-        elif binary_segment[:7]=="1110000":
-            decimal="7"
-        elif binary_segment[:7]=="1111111":
-            decimal="8"
-        elif binary_segment[:7]=="1111011":
-            decimal="9"
-        else:
-            print("Error")
-            return
-        if binary_segment[-1] == "1":
-            decimal+="."
-        ans+=decimal
-    if ans:
-        print(f"{float(ans.strip()):.2f}")
-    else:
+            binary+="0"
+    return binary
+def tran(binary):
+    """binary to decimal"""
+    result="Error"
+    if binary[:7]=="1111110":
+        result = "0"
+    if binary[:7]=="0110000":
+        result = "1"
+    if binary[:7]=="1101101":
+        result = "2"
+    if binary[:7]=="1111001":
+        result = "3"
+    if binary[:7]=="0110011":
+        result = "4"
+    if binary[:7]=="1011011":
+        result = "5"
+    if binary[:7]=="1011111":
+        result = "6"
+    if binary[:7]=="1110000":
+        result = "7"
+    if binary[:7]=="1111111":
+        result = "8"
+    if binary[:7]=="1111011":
+        result = "9"
+    return result
+def point(binary):
+    """point check"""
+    if binary[-1] == "1":
+        return "."
+    return ""
+def ans():
+    """print result"""
+    number=""
+    for _ in range(3):
+        binary=num1()
+        number+=tran(binary)+point(binary)
+    if number.count(".")>1 or "Error" in number:
         print("Error")
-binary1=num1()
-tran(binary1)
+    else:
+        print(f"{float(number):.2f}")
+ans()
