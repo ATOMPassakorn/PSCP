48b4f13e0ca960941cc44d9f0a49e2e8ece8f5d8
ATOMPassakorn <atom15440@gmail.com>
2024-09-23 17:54:38 +0700

    Life Is PSCP

--- Python/week12/3nPlus1.py
+++ Python/week12/3nPlus1.py
@@ -2,13 +2,14 @@
 def plus():
     """3nPlus1"""
     num=int(input())
-    count=0
     while num:
-        if not num%2:
-            num/=2
-            count+=1
-        else:
-            num=(num*3)+1
+        count=1
+        while num!=1:
+            if not num%2:
+                num/=2
+            else:
+                num=(num*3)+1
             count+=1
         print(count)
+        num=int(input())
 plus()
